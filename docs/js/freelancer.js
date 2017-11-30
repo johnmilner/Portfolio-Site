35726994f6aa251d253716f7993eb3a34a9ea76b
John <john.milner@mac.com>
2017-11-29 20:33:43 -0800

    add hidden footer

--- docs/js/freelancer.js
+++ docs/js/freelancer.js
@@ -38,6 +38,8 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  $('footer').footerReveal();
+
   // Floating label headings for the contact form
   $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
